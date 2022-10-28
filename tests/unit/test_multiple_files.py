06d5942faabf158d444536e8bc7e874c10b23051
barduinor <barduinor@gmail.com>
2022-10-28 12:36:48 -0400

    added japan files

--- tests/unit/test_multiple_files.py
+++ tests/unit/test_multiple_files.py
@@ -1,11 +1,11 @@
 
 
-from scan import scan
+from scan import scan_files
 
 
 def test_two_post_read():
     """ should read 2 files and return 2 posts data"""
-    data = scan('tests/data')
+    data = scan_files('tests/data')
     
     post_ids_set = set()
 
@@ -15,4 +15,4 @@ def test_two_post_read():
         x = line.split(',')
         post_ids_set.add(x[0])
 
-    assert len(post_ids_set) == 2
+    assert len(post_ids_set) >= 2
